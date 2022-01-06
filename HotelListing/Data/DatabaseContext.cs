cca748b83ec7d0332923028d7a36298e690eacb4
TJ Pittman <tj.pittman@eagleadvantage.com>
2022-01-06 17:40:11 -0500

    Added Database, AutoMapper, Unit of Work, and DTOs

---
+++ HotelListing/Data/DatabaseContext.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id=1,
+                    Name = "United States of America",
+                    ShortName = "US",
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Canada",
+                    ShortName = "CA",
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Mexico",
+                    ShortName = "MX",
+                }
+                );
+
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Hilton Hotel",
+                    Address = "Georgia",
+                    CountryId = 1,
+                    Rating = 4.5,
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Sandal Resort and Spa",
+                    Address = "Ontario",
+                    CountryId = 2,
+                    Rating = 4.0,
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Comfort Suites",
+                    Address = "Mexico City",
+                    CountryId = 1,
+                    Rating = 4.3,
+                }
+                ); ;
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        
+    }
+}
--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -1,7 +1,9 @@
+using HotelListing.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +29,9 @@ namespace HotelListing
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddDbContext<Data.DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+            );
 
             services.AddCors(c=>
             {
@@ -37,10 +41,14 @@ namespace HotelListing
                     .AllowAnyHeader());
             });
 
+            services.AddAutoMapper(typeof(MapperInitializer));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
